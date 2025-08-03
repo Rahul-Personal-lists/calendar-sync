5abe3b145c3900fa1028758dcaf629f7a5d75db6
Rahul <rahul@satel.ca>
2025-08-02 17:47:41 -0700

    Add Vercel config and temporarily disable PWA to test routing

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Temporarily disable PWA to test routing
+  // const withPWA = require("next-pwa")({
+  //   dest: "public",
+  //   register: true,
+  //   skipWaiting: true,
+  //   disable: process.env.NODE_ENV === "development",
+  // });
 };
 
-module.exports = withPWA(nextConfig); 
+module.exports = nextConfig; 
