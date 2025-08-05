5cc6f3fc0d67383ba6e9c9c9420ac1842644e380
Rahul <rahul@satel.ca>
2025-08-04 18:43:56 -0700

    add icon

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig = {
   reactStrictMode: true,
-  // Temporarily disable PWA to test routing
-  // const withPWA = require("next-pwa")({
-  //   dest: "public",
-  //   register: true,
-  //   skipWaiting: true,
-  //   disable: process.env.NODE_ENV === "development",
-  // });
 };
 
-module.exports = nextConfig; 
+module.exports = withPWA(nextConfig); 
